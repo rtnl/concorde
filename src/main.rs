23fdd86514b40604dcf875ab5ec4e159654cc44a
PolyVectors <PolyVector@protonmail.com>
2025-09-07 04:25:29 +0100

    struct and stuff

--- src/main.rs
+++ src/main.rs
@@ -7,19 +7,34 @@ use std::process::exit;
 use crate::axis::Axis;
 use crate::box2::Box2f;
 use crate::vec2::Vec2f;
+
 use x11rb::errors::ReplyError;
 use x11rb::{
     connection::Connection,
-    protocol::xproto::{ChangeWindowAttributesAux, ConnectionExt, EventMask, Screen},
     protocol::ErrorKind,
+    protocol::xproto::{ChangeWindowAttributesAux, ConnectionExt, EventMask, Screen},
 };
 
 struct Concorde<'a, C: Connection> {
     connection: &'a C,
+    screen: &'a Screen,
+    layout: Box2f,
 }
 
 impl<'a, C: Connection> Concorde<'a, C> {
-    // TODO: new
+    fn new(connection: &'a C, screen: &'a Screen) -> Self {
+        Self {
+            connection: connection,
+            screen: screen,
+            layout: Box2f::new(
+                Vec2f::zero(),
+                Vec2f::new(
+                    screen.width_in_pixels as f64,
+                    screen.height_in_pixels as f64,
+                ),
+            ),
+        }
+    }
 }
 
 fn manage_windows<C>(connection: &C, screen: &Screen) -> Result<(), ReplyError>
@@ -44,21 +59,13 @@ where
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let b = Box2f::new(Vec2f::zero(), Vec2f::new(1920.0, 1080.0));
-
-    let (l, r) = b.split(Axis::Horizontal, 0.25);
-    println!("{:?}", l);
-    println!("{:?}", r);
-
-    return Ok(());
-
     let Ok((connection, screen_number)) = x11rb::connect(None) else {
         eprintln!("Failed to connect to X server.");
         exit(1);
     };
 
-    let screen = &connection.setup().roots[screen_number];
-    manage_windows(&connection, screen)?;
+    let concorde = Concorde::new(&connection, &connection.setup().roots[screen_number]);
+    manage_windows(&connection, concorde.screen)?;
 
     loop {
         connection.flush()?;
